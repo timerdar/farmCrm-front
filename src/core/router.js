3d5efe8f01e85cce57099663f95d098fbb45c8d8
timerdar <timergazin24@mail.ru>
2025-07-16 23:49:39 +0500

    #20 - use regex in routes

--- src/core/router.js
+++ src/core/router.js
@@ -1,22 +1,40 @@
 import { Auth } from "../routes/Auth.js";
 import { Menu } from "../routes/Menu.js";
 import { NotFound } from "../routes/NotFound.js";
-import { UndoneOrdersConsumers } from "../routes/consumers/UndoneOrdersConsumers.js";
+import { Consumers } from "../routes/consumers/Consumers.js";
+import { Products } from "../routes/products/Products.js";
+import { Delivery } from "../routes/delivery/Delivery.js"
+import { OrdersOfConsumer } from "../routes/orders/OrdersOfConsumer.js";
 
 const routes = {
-    '/': Auth,
-    '/menu': Menu,
-    '/consumers': UndoneOrdersConsumers
+    '^/menu$': Menu,
+    '^/consumers/(.*?)/orders$': OrdersOfConsumer,
+    '^/consumers$': Consumers,
+    '^/products$': Products,
+    '^/delivery$': Delivery,
+    '^/$': Auth
 }
 
 export function router() {
-    const hash = location.hash.slice(1) || "/";
+    const hash = location.hash.slice(1);
 
-    const page = routes[hash] || NotFound;
+    var page = NotFound;
+
+    if (hash === '') {
+        page = Auth;
+    } else {
+        for (const route in routes) {
+            const regex = RegExp(route);
+            if (regex.test(hash)) {
+                page = routes[route];
+                break;
+            }
+        };
+    }
 
     const app = document.getElementById('app');
 
     app.innerHTML = '';
-    
+
     app.appendChild(page());
 };
